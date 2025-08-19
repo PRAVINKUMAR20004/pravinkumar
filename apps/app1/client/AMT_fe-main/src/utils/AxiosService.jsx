fa061c25cc135c5f2b5d878f346d3aebcb301a45
PRAVINKUMAR20004 <pravin2012004@gmail.com>
2025-08-19 12:45:43 +0530

    Initial commit for CI/CD with ArgoCD

---
+++ apps/app1/client/AMT_fe-main/src/utils/AxiosService.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const AxiosService = axios.create({
+    baseURL:"https://amt-be-2.onrender.com",
+    headers:{
+        "Content-Type":"application/json"
+    }
+})
+
+AxiosService.interceptors.request.use(config=>{
+    const token = sessionStorage.getItem('token')
+    if(config.authenticate && token)
+    {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
+
+export default AxiosService
